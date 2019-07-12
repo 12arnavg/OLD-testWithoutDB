ca13e91fbe10315bb9b90b237297f315aa373b04
Admin <Admin@Naveens-iMac.hsd1.ca.comcast.net>
2019-07-12 11:52:47 -0700

    Third commit

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("/hi")
 	public String sayHello() {
-		return "hello-2";
+		return "hello-3";
 	}
 }
